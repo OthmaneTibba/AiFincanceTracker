e064ec8e810963fc8fc084e22f7f66a252bb1239
OthmaneTibba <medtibba@gmail.com>
2024-07-15 09:48:41 +0900

    CategoryItemExpenseAnalytics

---
+++ AiFinanceTracker.Server.Functions/Dtos/CategoryItemExpenseAnalytics.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Newtonsoft.Json;
+
+namespace AiFinanceTracker.Server.Functions.Dtos
+{
+    public class CategoryItemExpenseAnalytics
+    {
+        [JsonProperty("category")]
+        public string Category { get; set; } = string.Empty;
+        [JsonProperty("amount")]
+        public double Amount { get; set; }
+    }
+}
